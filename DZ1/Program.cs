58f1c9041e9bcbf0c9f70e5a104b3127f04f5d5b
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-11-21 10:25:16 +0300

    sdgs

---
+++ DZ1/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+// Указание
+// Использовать рекурсию. Не использовать цикл.
+
+
+// Пример
+
+// N=5 => 1 2 3 4 5
+int number = Convert.ToInt32(Console.ReadLine());
+
+void Function(int number)
+{
+    if(1 == number)
+    {return ;}
+
+    number =  number - 1; 
+    Function(number);
+
+    Console.WriteLine(number);
+}
+
+Function(number);
+
+
+Console.WriteLine("Введите число");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите возведение в степень");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+
+int Function(int numberA, int numberB)
+{
+    if(1 == numberB)
+    {return numberA;}
+    return numberA * Function(numberA, numberB - 1);
+}
+//  Function( numberA, numberB);
+Console.Write("Итог = ");
+Console.WriteLine(Function(numberA, numberB));
