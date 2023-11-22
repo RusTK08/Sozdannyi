26ec0e7fcdd86c46090d067f60f2577db08b162e
Ruslan Romanov <ruslan97_1997@mail.ru>
2023-11-22 14:14:44 +0300

    dz2g

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -22,16 +22,3 @@ int Akkerman(int NumberM, int NumberN)
 }
 
 Console.Write(Akkerman(NumberM, NumberN));
-// Console.Write("Введите значение M: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите значение N: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-
-// int Akkerman(int m, int n)
-// {
-// if (m == 0) return n + 1;
-// else if (n == 0) return Akkerman(m - 1, 1);
-// else return Akkerman(m - 1, Akkerman(m, n - 1));
-// }
-
-// Console.Write($"Функция Аккермана равно {Akkerman(m, n)} ");
